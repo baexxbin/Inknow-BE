6a95088d86899e197ec6b1e2f0e3e083b98d980d
baexxbin <soo3886@gmail.com>
2023-04-26 18:01:45 +0900

    feat: Configure default files
    q

---
+++ src/main/java/InknowCorp/Inknow/InknowApplication.java
@@ -0,0 +1,12 @@
+package InknowCorp.Inknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class InknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InknowApplication.class, args);
+	}
+
+}
