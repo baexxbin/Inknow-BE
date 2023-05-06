ba6ef986ee31ae7b7c03805cf31b69e71c887be7
baexxbin <soo3886@gmail.com>
2023-05-06 14:18:48 +0900

    test: Add MemberServiceTest
    회원 가입, 중복 회원 테스트 코드 작성

---
+++ src/test/java/InknowCorp/Inknow/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package InknowCorp.Inknow.service;
+
+import InknowCorp.Inknow.domain.Member;
+import InknowCorp.Inknow.repository.MemberRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setHiddenName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOneMember(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception{
+        // given
+        Member member1 = new Member();
+        member1.setHiddenName("bae");
+
+        Member member2 = new Member();
+        member2.setHiddenName("bae");
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        // then
+        fail("예외가 발생해야 함");
+    }
+}
