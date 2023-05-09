30900d73172cea301b5a552fdeba02f361d59b3d
baexxbin <soo3886@gmail.com>
2023-05-09 14:20:55 +0900

    feat: Add creat method at Diary

--- src/main/java/InknowCorp/Inknow/domain/Diary.java
+++ src/main/java/InknowCorp/Inknow/domain/Diary.java
@@ -1,11 +1,12 @@
 package InknowCorp.Inknow.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter
+@Getter @Setter
 public class Diary {
     @Id @GeneratedValue
     @Column(name = "diary_id")
@@ -17,6 +18,16 @@ public class Diary {
     private String Content;
     private int heart;
     private int report;
+
+    //==생성 매소드==//
+    public static Diary createDiary(Write write, String content) {
+        Diary diary = new Diary();
+        diary.setWrite(write);
+        diary.setContent(content);
+        diary.setHeart(0);
+        diary.setReport(0);
+        return diary;
+    }
 
     /*
         비즈니스 로직
