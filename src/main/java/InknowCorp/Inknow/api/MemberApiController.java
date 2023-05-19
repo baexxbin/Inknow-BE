bc27297d4cf3d500e0f56dbe64df00da68e3f4f8
baexxbin <soo3886@gmail.com>
2023-05-19 15:49:59 +0900

    feat: Add MemberApiController
    회원 등록 api 생성

---
+++ src/main/java/InknowCorp/Inknow/api/MemberApiController.java
@@ -0,0 +1,40 @@
+package InknowCorp.Inknow.api;
+
+import InknowCorp.Inknow.domain.Member;
+import InknowCorp.Inknow.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/members")
+    public CreateMemberResponse saveMember(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setHiddenName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
